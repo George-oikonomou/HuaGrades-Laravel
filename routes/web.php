95262e10ac1aa80f262d6a033dae3b119f74229d
George Oikonomou <geroge.oik@gmail.com>
2025-07-22 00:24:21 +0300

    makes / home route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/home');
 
-Route::get('/home', [HomeController::class, 'home']);
+Route::get('/', [HomeController::class, 'home']);
 Route::get('/students/{student}/grades', [StudentController::class, 'grades']);
