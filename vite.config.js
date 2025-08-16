db49a58ef11e064ad29d639d15e85bb4bebf9de0
George-Oikonomou <geroge.oik@gmail.com>
2025-08-17 01:53:06 +0300

    hotfix , adds to vite the new css files

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/custom.css'
+                'resources/css/custom.css',
+                'resources/css/mobile.css',
+                'resources/css/statistics.css',
             ],
             refresh: true,
         }),
